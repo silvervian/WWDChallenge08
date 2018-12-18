ff844e6d6c5870c1d08ad33777e81f5e6aef52f2
Adijos <jedrczakadrian@gmail.com>
2018-12-19 00:52:15 +0100

    Half site done

--- gulpfile.js
+++ gulpfile.js
@@ -101,7 +101,7 @@ gulp.task('server', () => {
 });
 
 gulp.task('watch', ['default'], () => {
-  gulp.watch('src/sass/**/*.scss', ['sass']);
+  gulp.watch('src/sass/**/**/*.scss', ['sass']);
   gulp.watch('src/img/**/*.*', ['image']);
   gulp.watch('src/js/**/*.js', ['js']);
   gulp.watch('src/*.html', ['static']);
